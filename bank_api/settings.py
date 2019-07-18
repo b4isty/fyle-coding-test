d4a31df949c96994d0cbadfe6fd901a87a7f04e9
b4isty <baishakhidasgupta24@gmail.com>
2019-07-18 23:46:32 +0530

    static dir added

--- bank_api/settings.py
+++ bank_api/settings.py
@@ -82,24 +82,6 @@ WSGI_APPLICATION = 'bank_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': "django.db.backends.postgresql_psycopg2",
-#         'NAME': "bankdb",
-#         'USER': "db_user",
-#         'PASSWORD': "dbpass",
-#         'HOST': "localhost",
-#         'PORT': '',
-#     }
-# }
 
 DATABASES = {
     'default': {
